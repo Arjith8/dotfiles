c6b14a951691bafc8afa9fc9d862ff152093e202
Arjithar8 <arjithar8@gmail.com>
2025-02-18 02:32:17 +0530

    Testing copilot out

---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,15 @@
+return {
+    "github/copilot.vim",
+    lazy = false,
+    keys = {
+    },
+    setup = function()
+       -- vim.keymp.set("i", "<C-Tab>", 'copilot#Accept("\\<CR>")', {
+       --     expr = true,
+       --     replace_keycodes = false,
+       -- })
+       -- vim.g.copilot_no_tab_map = true
+       -- vim.g.copilot_no_auto_complete = true
+    end,
+
+}
