d084d0a1b90291f19550939294242f923d20b907
Arjithar8 <arjithar8@gmail.com>
2025-02-21 17:09:22 +0530

    Floatterm deleted, need to bring the git plugins and still havent figured out trouble

--- .config/nvim/lua/plugins/floatterm.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"voldikss/vim-floaterm",
-    lazy = true,
-    config = function ()
-        vim.keymap.set({"n", "t"}, "<M-t>", function() vim.cmd("FloatermToggle!") end)
-        vim.keymap.set({"n","t"}, "<M-n>", function () vim.cmd("FloatermNew!") end)
-        vim.keymap.set({"t"},"<leader>n" , function () vim.cmd("FloatermNext") end)
-    end
-}
