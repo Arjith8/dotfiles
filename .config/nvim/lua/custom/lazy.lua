009c8d58b1f453cf38288f1c7638c5e438967afa
Arjithar8 <arjithar8@gmail.com>
2025-02-21 15:57:43 +0530

    Added a keymap to toggle lazy window

--- .config/nvim/lua/custom/lazy.lua
+++ .config/nvim/lua/custom/lazy.lua
@@ -27,6 +27,7 @@ require("lazy").setup({
     -- import your plugins
     { import = "plugins" },
   },
+  vim.keymap.set("n", "<leader>ll", "<cmd>Lazy<CR>",{})
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
 --  install = { colorscheme = { "habamax" } },
