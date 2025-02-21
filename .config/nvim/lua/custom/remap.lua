c82a9f8adb4b91c80cd5fcdfe3ab865d19d3b76a
Arjithar8 <arjithar8@gmail.com>
2025-02-22 00:50:29 +0530

    Added some remaps to help with Trouble

--- .config/nvim/lua/custom/remap.lua
+++ .config/nvim/lua/custom/remap.lua
@@ -4,3 +4,8 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set({"v", "n"}, "<leader>y", "\"+y")
 vim.keymap.set({"v", "n"}, "<leader>Y", "\"+Y")
+
+vim .keymap.set("n", "<C-n>", "<C-w>w")
+vim.keymap.set("n", "<C-H>", "<cmd>vertical resize +5<CR>")
+vim.keymap.set("n", "<C-K>", "<cmd>resize +5<CR>")
+vim.keymap.set("n", "<C-J>", "<cmd>resize -5<CR>")
