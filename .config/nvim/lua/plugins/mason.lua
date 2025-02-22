7c45cb14c094469fc7dc97adfa3254ace2934c09
Arjithar8 <arjithar8@gmail.com>
2025-02-23 02:21:14 +0530

    Splitting mason from lsp, standard practice and helps with readability imo

---
+++ .config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,10 @@
+return {
+    "williamboman/mason.nvim",
+    dependencies = {
+        "stevearc/dressing.nvim",
+    },
+    opts = {},
+    keys = {
+        { '<leader>mp', "<cmd>Mason<cr>", desc = "Mason" },
+    },
+}
