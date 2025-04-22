9ba355390cbd69128bfaa61af32c670fd036cb3a
Arjithar8 <arjithar8@gmail.com>
2025-04-22 16:44:27 +0530

    New remaps

--- .config/nvim/lua/custom/remap.lua
+++ .config/nvim/lua/custom/remap.lua
@@ -9,6 +9,9 @@ vim .keymap.set("n", "<C-n>", "<C-w>w")
 vim.keymap.set("n", "<C-H>", "<cmd>vertical resize +5<CR>")
 vim.keymap.set("n", "<C-K>", "<cmd>resize +5<CR>")
 vim.keymap.set("n", "<C-J>", "<cmd>resize -5<CR>")
+vim.keymap.set("i", "jh", "<Esc>")
+vim.keymap.set("n", "<leader>q", "<cmd>q<CR>")
+
 
 vim.keymap.set("n", "<leader>ts", function ()
     local a = tonumber(vim.fn.input("Tap Space:"))
