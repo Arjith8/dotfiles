de722a2c2e873deb39ade8839bd443af13303b7a
Arjithar8 <arjithar8@gmail.com>
2026-01-19 14:19:46 +0530

    Formatting on cloak

--- .config/nvim/lua/plugins/cloak.lua
+++ .config/nvim/lua/plugins/cloak.lua
@@ -1,19 +1,19 @@
 return {
-    "laytan/cloak.nvim",
-    lazy = false,
-    opts = {
-        enabled = true,
-        cloak_character = "*",
-        highlight_group = "Comment",
-        patterns = {
-            {
-                file_pattern = { ".env*", "wrangler.toml", ".dev.vars" },
-                cloak_pattern = "=.+"
-            },
-        },
+  "laytan/cloak.nvim",
+  lazy = false,
+  opts = {
+    enabled = true,
+    cloak_character = "*",
+    highlight_group = "Comment",
+    patterns = {
+      {
+        file_pattern = { ".env*", "wrangler.toml", ".dev.vars" },
+        cloak_pattern = "=.+",
+      },
     },
-    keys = {
-        { "<leader>ct", "<cmd>CloakToggle<cr>", desc = "Cloak Toggle" },
-        { "<leader>cp", "<cmd>CloakPreviewLine<cr>", desc = "Cloak Preview Line"},
-    }
+  },
+  keys = {
+    { "<leader>ct", "<cmd>CloakToggle<cr>", desc = "Cloak Toggle" },
+    { "<leader>cp", "<cmd>CloakPreviewLine<cr>", desc = "Cloak Preview Line" },
+  },
 }
