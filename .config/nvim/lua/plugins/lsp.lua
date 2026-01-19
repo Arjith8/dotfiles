ab6cc3115af43ab93f2dc64e9e7cb92dfa4133a9
Arjithar8 <arjithar8@gmail.com>
2026-01-19 10:53:55 +0530

    Doesnt remeber making any changes to lsp ig its just formatting

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,91 +1,91 @@
 return {
-    {
-        'saghen/blink.cmp',
-        dependencies = { 'rafamadriz/friendly-snippets' },
-        version = '1.*',
-        ---@module 'blink.cmp'
-        ---@type blink.cmp.Config
-        opts = {
-            -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-            -- 'super-tab' for mappings similar to vscode (tab to accept)
-            -- 'enter' for enter to accept
-            -- 'none' for no mappings
-            --
-            -- All presets have the following mappings:
-            -- C-space: Open menu or open docs if already open
-            -- C-n/C-p or Up/Down: Select next/previous item
-            -- C-e: Hide menu
-            -- C-k: Toggle signature help (if signature.enabled = true)
-            --
-            -- See :h blink-cmp-config-keymap for defining your own keymap
-            keymap = { 
-                preset = 'default',
-                ["<M-CR>"] = { 'select_and_accept'},
-            },
+  {
+    "saghen/blink.cmp",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    version = "1.*",
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+      -- 'super-tab' for mappings similar to vscode (tab to accept)
+      -- 'enter' for enter to accept
+      -- 'none' for no mappings
+      --
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-n/C-p or Up/Down: Select next/previous item
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      --
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      keymap = {
+        preset = "default",
+        ["<M-CR>"] = { "select_and_accept" },
+      },
 
-            appearance = {
-                -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-                -- Adjusts spacing to ensure icons are aligned
-                nerd_font_variant = 'mono'
-            },
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = "mono",
+      },
 
-            completion = {
-                documentation = {
-                    auto_show = true,
-                },
-                menu = {
-                    draw = {
-                        components = {
-                            kind_icon = {
-                                highlight = function(ctx)
-                                    -- return (
-                                    -- require("blink.cmp.completion.windows.render.tailwind").get_hl(ctx)
-                                    -- or "BlinkCmpKind"
-                                    -- ) .. ctx.kind
-                                    local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
-                                    return hl
-                                    end,
-                                ellipsis = false,
-                                text = function(ctx)
-                                    local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
-                                    return kind_icon
-                                end,
-                            }
-                        },
-                    },
-                }
-            },
-
-            -- Default list of enabled providers defined so that you can extend it
-            -- elsewhere in your config, without redefining it, due to `opts_extend`
-            sources = {
-                default = { 'lsp', 'path', 'snippets', 'buffer' },
-            },
-
-            -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-            -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-            -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-            --
-            -- See the fuzzy documentation for more information
-            fuzzy = { implementation = "prefer_rust_with_warning" }
+      completion = {
+        documentation = {
+          auto_show = true,
         },
-        opts_extend = { "sources.default" },
-        signature = {
-            enabled = true,
-            trigger = {
-                enabled = true,
-                show_on_keyword = true,
-            }
-        }
+        menu = {
+          draw = {
+            components = {
+              kind_icon = {
+                highlight = function(ctx)
+                  -- return (
+                  -- require("blink.cmp.completion.windows.render.tailwind").get_hl(ctx)
+                  -- or "BlinkCmpKind"
+                  -- ) .. ctx.kind
+                  local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                  return hl
+                end,
+                ellipsis = false,
+                text = function(ctx)
+                  local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+                  return kind_icon
+                end,
+              },
+            },
+          },
+        },
+      },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+
+      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+      --
+      -- See the fuzzy documentation for more information
+      fuzzy = { implementation = "prefer_rust_with_warning" },
     },
-    { 'echasnovski/mini.icons', version = false },
-    {
-        "mason-org/mason-lspconfig.nvim",
-        dependencies = {
-            "mason.nvim",
-            "neovim/nvim-lspconfig",
-            "saghen/blink.cmp",
-        },
-        opts = {}
-    }
+    opts_extend = { "sources.default" },
+    signature = {
+      enabled = true,
+      trigger = {
+        enabled = true,
+        show_on_keyword = true,
+      },
+    },
+  },
+  { "echasnovski/mini.icons", version = false },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      "mason.nvim",
+      "neovim/nvim-lspconfig",
+      "saghen/blink.cmp",
+    },
+    opts = {},
+  },
 }
