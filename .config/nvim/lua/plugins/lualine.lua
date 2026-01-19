5c68c1d65abcdeeefbaa6c036db686442d7086ba
Arjithar8 <arjithar8@gmail.com>
2026-01-19 10:51:20 +0530

    Updated lualine to show copilot status

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,5 +1,26 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {}
+  "nvim-lualine/lualine.nvim",
+  dependencies = { 'nvim-tree/nvim-web-devicons'},
+  opts = function(_, opts)
+    opts.sections = opts.sections or {}
+    opts.sections.lualine_c = opts.sections.lualine_c or {}
+
+    -- Copilot status
+    table.insert(opts.sections.lualine_c, {
+      function()
+        return " "
+      end,
+      color = function()
+        local status = require("sidekick.status").get()
+        if status then
+          return status.kind == "Error" and "DiagnosticError" or status.busy and "DiagnosticWarn" or "Special"
+        end
+      end,
+      cond = function()
+        local status = require("sidekick.status")
+        return status.get() ~= nil
+      end,
+    })
+
+  end,
 }
