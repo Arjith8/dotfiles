7c43324e063fd86f6ea957ba1c30d4ca317da4c4
Arjithar8 <arjithar8@gmail.com>
2026-01-20 13:26:37 +0530

    All ai things removed mini-ai is not ai but still reoved it

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -5,32 +5,5 @@ return {
     opts.sections = opts.sections or {}
     opts.sections.lualine_c = opts.sections.lualine_c or {}
     opts.sections.lualine_y = opts.sections.lualine_y or {}
-
-    -- Copilot status
-    table.insert(opts.sections.lualine_c, {
-      function()
-        return " "
-      end,
-      color = function()
-        local status = require("sidekick.status").get()
-        if status then
-          return status.kind == "Error" and "DiagnosticError" or status.busy and "DiagnosticWarn" or "Special"
-        end
-      end,
-      cond = function()
-        local status = require("sidekick.status")
-        return status.get() ~= nil
-      end,
-    })
-
-    table.insert(opts.sections.lualine_y, {
-      function()
-        return " "
-      end,
-      cond = function()
-        local status = require("supermaven-nvim.api").is_running()
-        return status == true
-      end,
-    })
   end,
 }
