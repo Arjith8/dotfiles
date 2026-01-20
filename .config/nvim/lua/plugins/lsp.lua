9049e1c29bcbf5ce7aacd715612d32e7e03765f6
Arjithar8 <arjithar8@gmail.com>
2026-01-20 13:24:39 +0530

    Base from with AI will be removed

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -6,11 +6,6 @@ return {
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-      -- 'super-tab' for mappings similar to vscode (tab to accept)
-      -- 'enter' for enter to accept
-      -- 'none' for no mappings
-      --
       -- All presets have the following mappings:
       -- C-space: Open menu or open docs if already open
       -- C-n/C-p or Up/Down: Select next/previous item
@@ -20,12 +15,15 @@ return {
       -- See :h blink-cmp-config-keymap for defining your own keymap
       keymap = {
         preset = "default",
-        ["<M-CR>"] = { "select_and_accept" },
+        ["<C-y>"] = {
+          function(cmp)
+            cmp.select_and_accept()
+            cmp.show_signature()
+          end,
+        },
       },
 
       appearance = {
-        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
         nerd_font_variant = "mono",
       },
 
