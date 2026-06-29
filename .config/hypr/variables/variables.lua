e091bf51db4c42650d9e24d78b680de472738807
Arjithar8 <122336800+Arjith8@users.noreply.github.com>
2026-06-30 05:12:10 +0530

    cleaning up unnecessary local variables

--- .config/hypr/variables/variables.lua
+++ .config/hypr/variables/variables.lua
@@ -1,7 +1,4 @@
-local user_apps = require("variables.user-apps")
-local mainMod = "SUPER"
-
 return {
-    mainMod = mainMod,
-    user_apps = user_apps
+    mainMod = "SUPER",
+    user_apps = require("variables.user-apps")
 }
