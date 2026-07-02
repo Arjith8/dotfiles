330def103152d019671d977d523ccf300bb85c6e
Arjithar8 <122336800+Arjith8@users.noreply.github.com>
2026-07-02 12:29:17 +0530

    updated default filemager and menu bar

--- .config/hypr/variables/user-apps.lua
+++ .config/hypr/variables/user-apps.lua
@@ -1,6 +1,6 @@
 local terminal    = "ghostty"
-local fileManager = "dolphin"
-local menu        = "hyprlauncher"
+local fileManager = "nautilus"
+local menu        = "rofi -show run"
 local browser     = "flatpak run app.zen_browser.zen"
 
 return {
